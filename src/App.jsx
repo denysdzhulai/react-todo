9004ae2239e34ef67ee1623e15bc404cceba0cde
Denys Dzhulai <denisjuly@gmail.com>
2024-12-08 19:01:48 -0700

    Implemented custom hook and localStorage for Todo app; enhance UI design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+function useSemiPersistentState(key, initialState) {
+  const [state, setState] = React.useState(() => {
+    const savedState = localStorage.getItem(key);
+    return savedState ? JSON.parse(savedState) : initialState;
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
+
 const App = () => {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", []);
 
   const addTodo = (newTodo) => {
     setTodoList((prevList) => [...prevList, newTodo]);
   };
 
   return (
-    <div>
+    <>
       <h1>Todo App</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 };
 
